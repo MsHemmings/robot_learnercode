338942bb32b8569d2cc2e9f5942943d2afc5bcd6
MsHemmings <110316449+MsHemmings@users.noreply.github.com>
2022-07-31 14:17:27 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedNumberDeprecated(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.analogWritePin(AnalogPin.P14, 0)
+        pins.analogWritePin(AnalogPin.P16, 0)
+    } else if (receivedNumber == 0) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    } else if (receivedNumber == 0) {
+    	
+    } else {
+    	
+    }
+})
+radio.setGroup(1)
+let receivedNumber = 0
